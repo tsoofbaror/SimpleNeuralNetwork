eee08fb6719089bfb5debb98e0ee07b027a02b5b
Tsoof Laptop <tsoofbaror@gmail.com>
2023-06-07 18:52:56 +0300

    final for posting

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# SimpleNeuralNetwork
+# Simple Neural Network
+This is a basic neural network implementation on this dataset from Kaggle: https://www.kaggle.com/datasets/teejmahal20/airline-passenger-satisfaction
+
+Built by Tsoof Bar-Or for educational purposes.
+https://twitter.com/tsoofbaror
+
+
--- main.py
+++ main.py
@@ -34,40 +34,37 @@ class NeuralNetwork(nn.Module):
             x = layer(x)
         return x
 
-
+# הוצאת סט אימון, אימות ובחינה
 train_x, train_y, val_x, val_y, test_x, test_y, mapping_dict = preprocess("train.csv", "test.csv")
-print(mapping_dict)
-exit()
+
+# העברה של המידע לכרטיס המסך
 train_x = train_x.to(device)
 train_y = train_y.to(device)
 val_x = val_x.to(device)
 val_y = val_y.to(device)
+test_x = test_x.to(device)
+test_y = test_y.to(device)
 
-# Set the hyperparameters
+# הגדרת היפרפמטרים
 input_size = train_x.shape[1]
-hidden_size = [256, 1024, 1024, 512, 32]
-num_classes = 1
+hidden_size = [1]
+num_outputs = 1
 learning_rate = 0.0001
 num_epochs = 100
-# Set your batch size
 batch_size = 516
 
-# Compute the number of batches
+# הגדרת מספר הבאצ'ים
 n_batches = int(np.ceil(len(train_x) / batch_size))
 
-# Create the neural network object
-model = NeuralNetwork(input_size, hidden_size, num_classes).to(device)
+# יצירת רשת הנוירונים
+model = NeuralNetwork(input_size, hidden_size, num_outputs).to(device)
 
-# Define the loss function and optimizer
+# הגדרת לוס ואופטימייזר
 criterion = nn.BCEWithLogitsLoss()
 optimizer = optim.Adam(model.parameters(), lr=learning_rate)
 
-# Define the lists to store the results
-train_losses = []
-val_losses = []
-val_accs = []
 
-# Define the lists to store the results
+# רשימות לשמור את התוצאות
 train_losses = []
 train_accs = []
 val_losses = []
@@ -75,59 +72,67 @@ val_accs = []
 
 for epoch in range(num_epochs):
 
-    # Shuffle your data at the start of each epoch
+    # מערבבים את הדאטה
     indices = np.arange(len(train_x))
     np.random.shuffle(indices)
     train_x = train_x[indices]
     train_y = train_y[indices]
 
     for i in range(n_batches):
-        # Compute the start and end indices for the current batch
+        # חישוב מיקום הדגימה הנוכחית
         start = i * batch_size
         end = min(start + batch_size, len(train_x))
 
-        # Extract the current batch
+        # הגדרת הבאץ'
         batch_x = train_x[start:end]
         batch_y = train_y[start:end]
 
-        # Forward pass
+        # העברה שלו ברשת
         outputs = model(batch_x)
         loss = criterion(outputs.squeeze(1), batch_y)
 
-        # Backward and optimize
+        # חישוב גרדיאנט וביצוע צעד של - Gradient Descent
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
 
-    # Evaluate on validation set after each epoch
+    # נבחן את המודל אחרי כל אפוק
     model.eval()
     with torch.no_grad():
-        # Compute training accuracy
+        # חישוב הדיוק על סט האימון
         outputs = model(train_x).squeeze(1)
         train_preds = (torch.sigmoid(outputs) > 0.5).float()
         train_correct = (train_preds == train_y).sum().item()
         train_acc = train_correct / len(train_y)
 
+        # חישוב הדיוק והלוס על סט האימות
         val_outputs = model(val_x).squeeze(1)
         val_loss = criterion(val_outputs, val_y)
-
-        # Convert outputs to predicted class
         val_preds = (val_outputs > 0.5).float()  # Changed this line
         val_correct = (val_preds == val_y).sum().item()
         val_acc = val_correct / len(val_y)
-
-    # Switch back to training mode
     model.train()
 
-    # Store losses and accuracy
+    # שמירת לוס ודיוק
     train_accs.append(train_acc)
     train_losses.append(loss.item())
     val_losses.append(val_loss.item())
     val_accs.append(val_acc)
 
-    # Print the validation loss and accuracy after each epoch
+    # הדפסה בסוף כל אפוק
     print(
         f"Epoch {epoch + 1}/{num_epochs}, Train Loss: {loss.item():.4f}, Train Acc: {train_acc:.4f}, Val Loss: {val_loss.item():.4f}, Val Acc: {val_acc:.4f}")
 
-# Plot the training losses, validation losses, and validation accuracies
+# חישוב הדיוק על סט הבחינה
+model.eval()
+with torch.no_grad():
+    test_outputs = model(test_x).squeeze(1)
+    test_loss = criterion(test_outputs, test_y)
+    test_preds = (torch.sigmoid(test_outputs) > 0.5).float()
+    test_correct = (test_preds == test_y).sum().item()
+    test_acc = test_correct / len(test_y)
+
+print(f'Test Accuracy: {test_acc * 100:.2f}%')
+
+# ציור התוצאות
 plot_results(train_losses, val_losses, val_accs, train_accs)
